593d54dd38341ab6a170cf88dcbef52caa39f7f2
lukas <komprs.l@gmail.com>
2021-12-11 16:47:28 +0100

    SERVER-SIDE-RENDERING Streaming Markup, sending the response to the user in stream, pieces

--- Server-side-rendering/server/index.js
+++ Server-side-rendering/server/index.js
@@ -1,6 +1,6 @@
 import React from 'react-dom'
 import express from "express";
-import { renderToString } from "react-dom/server";
+import { renderToNodeStream } from "react-dom/server";
 import { StaticRouter } from "react-router-dom";
 import fs from "fs";
 import App from "../src/App";
@@ -15,6 +15,9 @@ const app = express();
 
 app.use("/dist", express.static("dist"));
 app.use((req, res) => {
+  //First piece we have alredeay prerendered coz it is coming fro index.html
+  //Now the user is going to get immediately their CSS and everything else in the head tag, So we will start donwloading stuff immediaty
+  res.write(parts[0]);
   const staticContext = {};
   const reactMarkup = (
     <StaticRouter url={req.url} context={staticContext}>
@@ -22,10 +25,19 @@ app.use((req, res) => {
     </StaticRouter>
   );
 
-  res.status(staticContext.statusCode || 200);
-  res.send(`${parts[0]}${renderToString(reactMarkup)}${parts[1]}`);
-  res.end();
+  // this stream is hooked up to my response to the user parts[0], 
+  const stream = renderToNodeStream(reactMarkup);
+  stream.pipe(res, { end: false });
+  // once the react app fully rendered  we can say
+  stream.on("end", () => {
+    res.status(staticContext || 200);
+    res.write(parts[1]);
+    res.end();
+  })
 });
 
 console.log(`listening on http://localhost:${PORT}`);
 app.listen(PORT);
+
+//No to use server-side rendering for other components we can do
+// app.use(express.static("dist/index.html"));
